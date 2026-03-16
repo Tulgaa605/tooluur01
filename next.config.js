6d2c37408e5d16d4990c8a26269b4d7fb4cab143
Tulgaa <munkhtulga0118@gmail.com>
2026-03-16 14:22:02 +0800

    shine

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = config.watchOptions || {};
+      config.watchOptions.ignored = [
+        ...(Array.isArray(config.watchOptions.ignored) ? config.watchOptions.ignored : [config.watchOptions.ignored].filter(Boolean)),
+        '**/node_modules/**',
+        '**/.next/**',
+        'C:\\System Volume Information/**',
+        'C:\\pagefile.sys',
+      ].filter(Boolean);
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
